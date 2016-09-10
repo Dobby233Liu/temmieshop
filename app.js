7ecef52242e77c18bfa1e46430a541721f63b2f4
Michael Leonffu <leonffu.michael@gmail.com>
2016-09-10 16:57:13 -0700

    Susessfully installeed temmie!
    woot!

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
+//app.get('/', function (req, res) {
+//  res.send(index.html);
+//});
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.use(express.static('public'));
+
+app.listen(13409, function () {
+  console.log('Example app listening on port 13409!');
 });
